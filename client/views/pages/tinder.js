76c1664efa8c9687490ed0e76aba3d1994595942
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-11-21 19:44:24 -0500

    trying to add swipe event

--- client/views/pages/tinder.js
+++ client/views/pages/tinder.js
@@ -2,5 +2,8 @@ Template.tinder.helpers({
   randomUser: function(){
     console.log("randomUser");
     return Meteor.users.findOne();
+  },
+  'swipe click': function(){
+    console.log("clicked swipe link");
   }
 });
